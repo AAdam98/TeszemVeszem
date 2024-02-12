8eeca9a3847231bf8e15892d6e4cf49f8ebb69eb
marklazanyecz <lazikaa1@gmail.com>
2024-02-12 10:09:53 +0100

    database 0.1.1

--- website/models.py
+++ website/models.py
@@ -6,18 +6,22 @@ class Advertisement(db.Model):
     advertisementID = db.Column(db.Integer, primary_key=True)
     userID = db.Column(db.Integer, db.ForeignKey)
     category = db.Column(db.String, nullable=False)
-    avaible = db.Column(db.Boolean, default=True)
+    available = db.Column(db.Boolean, default=True)
     title = db.Column(db.String(60), nullable=False)
     description = db.Column(db.String(1000))
     price = db.Column(db.Integer(7), nullable=False)
-    date = db.Column(db.Date) 
+    date = db.Column(db.Date)
+
+    user = db.relationship('User', backref='advertisements', foreign_keys=[userID])
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40))
+    userID = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(50))
+    def get_id(self):
+        return (self.userID)
 
 
 class Comment(db.Model):
@@ -26,3 +30,6 @@ class Comment(db.Model):
     advertisementID = db.Column(db.Integer, db.ForeignKey)
     content = db.Column(db.String(300), nullable=False)
     date = db.Column(db.Date)
+
+    user = db.relationship('User', backref='comments', foreign_keys=[userID])
+    advertisement = db.relationship('Advertisement', backref='comments', foreign_keys=[advertisementID])
