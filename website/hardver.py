8f40a970ecc28323f49798373c4e2f746b61e2fa
marklazanyecz <lazikaa1@gmail.com>
2024-02-14 13:23:50 +0100

    updated queries

--- website/hardver.py
+++ website/hardver.py
@@ -11,7 +11,17 @@ def index():
     advertisements = session.query(Advertisement).all()
     return 'ez az összes hardver', advertisements
 
-@hardver.route("/<category>")
-def index(category):
+@hardver.route("/<category>", methods=["GET", "POST"])
+def query(category, min, max, order):
+    if request.method == "POST":
+        #szűrés felhasználó által megadott ár alapján 
+        query = session.query(Advertisement).filter_by(category=category)
+        if min is not None:
+            query = query.filter(Advertisement.price >= min)
+        if max is not None:
+            query = query.filter(Advertisement.price <= max)
+        filtered_advertisements = query.all()
+        return filtered_advertisements
+    
     filtered_advertisements = session.query(Advertisement).filter_by(category=category).all()
     return 'ez az összes hardver', filtered_advertisements
