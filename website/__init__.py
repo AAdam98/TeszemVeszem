924e5028e2148edf57f0d8a5e40ef001e82a90dd
Szarvas Ádám <101208633+AAdam98@users.noreply.github.com>
2024-02-28 13:03:58 +0100

    __init__ adatbázisba ikonok elérési útvonala beszúrva

--- website/__init__.py
+++ website/__init__.py
@@ -52,7 +52,9 @@ def create_database(app):
             else:
                 admin_user = User(email=email, username = username, password=generate_password_hash(password,method='pbkdf2:sha256'), is_admin=True)
                 db.session.add(admin_user)
-                categories = [("Alaplap", "static/icons/alaplap.svg"),
+                
+                categories = [
+                            ("Alaplap", "static/icons/alaplap.svg"),
                             ("Processzor","static/icons/processzor.svg"),
                             ("Memória","static/icons/memoria.svg"),
                             ("Hűtés","static/icons/hutes.svg"),
@@ -63,13 +65,14 @@ def create_database(app):
                             ("Egyéb hardverek","static/icons/egyebhardver.svg"),
                             ("Retró hardverek","static/icons/retrohardver.svg"),
                             ("Videókártya","static/icons/videkartya.svg"),
-                            ("Monitor","Merevlemez, SSD","static/icons/monitor.svg"),
+                            ("Monitor","static/icons/monitor.svg"),
+                            ("Merevlemez, SSD","static/icons/merevlemezssd.svg"),
                             ("Adathordozó","static/icons/adathordozo.svg"),
                             ("Hálózati termékek","static/icons/halozati.svg"),
                             ("Nyomtató, szkenner", "static/icons/nyomtato.svg")
                             ]
-                for category in categories:
-                    newCat = Category(name=category)
+                for name, icon_path in categories:
+                    newCat = Category(name=name, icon_path=icon_path)
                     db.session.add(newCat)
             db.session.commit()
         print('Adatbázis létrehozva!')
