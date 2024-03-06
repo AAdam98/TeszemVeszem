954d1860d35c32062a8d6a2c49f77975ab235d5c
marklazanyecz <lazikaa1@gmail.com>
2024-03-06 09:26:39 +0100

    jelszómódosítás javítva

--- website/views.py
+++ website/views.py
@@ -29,30 +29,35 @@ def home():
 def adatlap():
     return render_template("profile.html", user=current_user)
 
-@views.route('/password', methods=['GET','PUT'])
+@views.route('/password', methods=['GET', 'PUT', 'POST'])
 @login_required
 def password():
     user = current_user
-    if request.method == 'PUT':
+
+    if request.method == 'POST' or (request.method == 'GET' and request.args.get('_method') == 'PUT'):
         curpassw = request.form.get('curPassw')
+        
         if check_password_hash(user.password, curpassw):
             newpassw1 = request.form.get('newPassw1')
             newpassw2 = request.form.get('newPassw2')
-            if newpassw1 == newpassw2:
+
+            if not newpassw1 and not newpassw2:
+                flash('Az új jelszó mezők nem lehetnek üresek!', category='error')
+            elif newpassw1 == newpassw2:
                 if curpassw == newpassw1:
                     flash("A jelenlegi és az új jelszó megegyezik!", category='error')
+                elif len(newpassw1) < 6:
+                    flash('Az új jelszó túl rövid!', category='error')
                 else:
-                    if len(newpassw1) < 6:
-                        flash('Az új jelszó túl rövid!', category='error')
-                    else:
-                        user.password = generate_password_hash(newpassw1,method='pbkdf2:sha256')
-                        db.session.commit()
-                        flash('A jelszavad sikeresen megváltoztattad')
-                        return redirect(url_for('views.home'))
+                    user.password = generate_password_hash(newpassw1, method='pbkdf2:sha256')
+                    db.session.commit()
+                    flash('A jelszavad sikeresen megváltoztattad')
+                    return redirect(url_for('views.home'))
             else:
                 flash('A megadott jelszavak nem egyeznek', category='error')
         else:
             flash('Hibás jelszó', category='error')
+
     return render_template("profile_passw.html", user=current_user)
 
 @views.route('/aboutus')
