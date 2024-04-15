5fda59993ff244ac38f123e26f5803e855aeb561
Adam <aadam.public@gmail.com>
2024-04-15 12:32:51 +0200

    hibajavítások

--- website/static/script.js
+++ website/static/script.js
@@ -1,5 +1,5 @@
 function redirectToPage(url) {
-	window.location.href = url;
+    window.location.href = url;
 }
 
 function submitForm() {
@@ -11,3 +11,23 @@ function confirmDelete() {
         document.getElementById("deleteForm").submit();
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var inputElement = document.getElementById("image");
+    var previewImageElement = document.getElementById("preview");
+
+    inputElement.addEventListener("change", function (event) {
+        var file = event.target.files[0];
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                previewImageElement.src = e.target.result;
+                previewImageElement.style.display = "block"; // Megjelenítjük az előnézetet
+            }
+            reader.readAsDataURL(file);
+
+            // Frissítjük a rejtett input mezőt a kiválasztott fájl nevével
+            document.getElementById("preview_image").value = file.name;
+        }
+    });
+});
